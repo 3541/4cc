c2c4f9cd29a5518df8d2e6f9b5ea9451081966f4
3541 <alex@marxist.party>
2019-10-29 00:31:01 +1100

    Lexer for basic subset of C

---
+++ src/main.rs
@@ -0,0 +1,95 @@
+extern crate peeking_take_while;
+
+use peeking_take_while::PeekableExt;
+
+#[derive(Debug)]
+enum Token {
+    OpenBrace,
+    CloseBrace,
+    OpenParenthesis,
+    CloseParenthesis,
+    Semicolon,
+    Keyword(Keyword),
+    Identifier(String),
+    Literal(Literal),
+    Unidentified,
+}
+
+#[derive(Debug)]
+enum Keyword {
+    Int,
+    Return,
+}
+
+#[derive(Debug)]
+enum Literal {
+    Int(u32),
+}
+
+fn lex(s: &str) -> Vec<Token> {
+    let mut tok = Vec::new();
+    let mut it = s
+        .chars()
+        .skip_while(|&c| c == ' ' || c == '\t' || c == '\n')
+        .peekable();
+    while let Some(c) = it.peek() {
+        //        print!("{} =>", c);
+        tok.push(match c {
+            '{' => {
+                it.next();
+                Token::OpenBrace
+            }
+            '}' => {
+                it.next();
+                Token::CloseBrace
+            }
+            '(' => {
+                it.next();
+                Token::OpenParenthesis
+            }
+            ')' => {
+                it.next();
+                Token::CloseParenthesis
+            }
+            ';' => {
+                it.next();
+                Token::Semicolon
+            }
+            'A'..='Z' | 'a'..='z' => {
+                match it
+                    .by_ref()
+                    .peeking_take_while(|&c| (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+                    .collect::<String>()
+                    .as_ref()
+                {
+                    "int" => Token::Keyword(Keyword::Int),
+                    "return" => Token::Keyword(Keyword::Return),
+                    s => Token::Identifier(String::from(s)),
+                }
+            }
+            '0'..='9' => Token::Literal(Literal::Int(
+                it.by_ref()
+                    .peeking_take_while(|&c| c >= '0' && c <= '9')
+                    .collect::<String>()
+                    .parse()
+                    .expect("Failed to parse integer literal"),
+            )),
+            _ => {
+                it.next();
+                Token::Unidentified
+            }
+        });
+        it.by_ref()
+            .peeking_take_while(|&c| c == '\t' || c == ' ' || c == '\n')
+            .last();
+        //        println!("Got {:?}", tok.last().unwrap());
+    }
+    tok
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let src = std::fs::read_to_string(&args[1]).unwrap();
+    let tok = lex(&src);
+    println!("{:#?}", tok);
+}
