4511a67e31c9687c5df3150d09c3b3cd51623346
Alex O'Brien <3541@3541.website>
2022-08-03 13:59:50 +1000

    ERROR: Colorize error messages.

--- src/error.c
+++ src/error.c
@@ -11,9 +11,31 @@
 
 #include <stdarg.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #include <a3/str.h>
 #include <a3/util.h>
+
+typedef enum Color {
+    C_RED,
+    C_NONE,
+} Color;
+
+#define COLOR_ESCAPE(C) "\033[" #C "m"
+
+static char const* color(Color color) {
+    if (!isatty(STDERR_FILENO) || getenv("NO_COLOR") || !(getenv("COLOR") || getenv("COLORTERM")))
+        return "";
+
+    switch (color) {
+    case C_RED:
+        return COLOR_ESCAPE(31);
+    case C_NONE:
+        return COLOR_ESCAPE(0);
+    }
+
+    A3_UNREACHABLE();
+}
 
 A3_FORMAT_FN(2, 0)
 static void verror_at_eof(A3CString src, char* fmt, va_list args) {
@@ -54,7 +76,7 @@ void verror_at(A3CString src, Span span, char* fmt, va_list args) {
     if (chunk.ptr[chunk.len - 1] == '\n')
         chunk.len--;
 
-    fprintf(stderr, "Error (%zu):\n", span.line);
+    fprintf(stderr, "%sError%s (%zu):\n", color(C_RED), color(C_NONE), span.line);
     do {
         size_t offset = 0;
         while (isspace(chunk.ptr[offset]) && chunk.ptr[offset] != '\n')
@@ -69,18 +91,33 @@ void verror_at(A3CString src, Span span, char* fmt, va_list args) {
 
         A3CString whitespace = { .ptr = chunk.ptr, .len = offset };
         A3CString line       = { .ptr = chunk.ptr, .len = len };
-        fprintf(stderr, A3_S_F "\n" A3_S_F, A3_S_FORMAT(line), A3_S_FORMAT(whitespace));
+
+        A3CString line_before_span = { .ptr = chunk.ptr,
+                                       .len = line.ptr < span.text.ptr
+                                                  ? MIN(len, (size_t)(span.text.ptr - line.ptr))
+                                                  : 0 };
+        A3CString line_after_span  = { .ptr = span.text.ptr + span.text.len,
+                                       .len = line_before_span.len + span.text.len < len
+                                                  ? len - span.text.len - line_before_span.len
+                                                  : 0 };
+        A3CString line_in_span     = { .ptr = chunk.ptr + line_before_span.len,
+                                       .len = MIN(span.text.len, len - line_before_span.len) };
+
+        fprintf(stderr, A3_S_F "%s" A3_S_F "%s" A3_S_F "\n" A3_S_F, A3_S_FORMAT(line_before_span),
+                color(C_RED), A3_S_FORMAT(line_in_span), color(C_NONE),
+                A3_S_FORMAT(line_after_span), A3_S_FORMAT(whitespace));
 
         if (line.ptr + line.len >= span.text.ptr) {
             int caret_offset = line.ptr < span.text.ptr
                                    ? (int)((size_t)(span.text.ptr - line.ptr) - whitespace.len)
                                    : 0;
             assert(caret_offset >= 0);
-            fprintf(stderr, "%*s", caret_offset, "");
+            fprintf(stderr, "%*s%s", caret_offset, "", color(C_RED));
             for (size_t j = 0; j < len - offset - (size_t)caret_offset &&
                                line.ptr + j + caret_offset < span.text.ptr + span.text.len - 1;
                  j++)
                 fputc('^', stderr);
+            fprintf(stderr, "%s", color(C_NONE));
 
             if (line.ptr + line.len >= span.text.ptr + span.text.len) {
                 fputc(' ', stderr);
