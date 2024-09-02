5d3a857e72a1a25a638ffffca83e9cd2d8c5b4d5
Alex O'Brien <3541@3541.website>
2024-09-02 18:54:51 +1000

    TEST: Expand compound literal test.

--- test/unit/expr/compound_literal.c
+++ test/unit/expr/compound_literal.c
@@ -13,4 +13,8 @@ struct S* f(struct S* s) {
 int main(void) {
     struct S* s = f(&(struct S) { 2 });
     assert(s->x == 6);
+
+    struct S* t = f(&(struct S) { 8 });
+    assert(t->x == 12);
+    assert(s->x == 6);
 }
